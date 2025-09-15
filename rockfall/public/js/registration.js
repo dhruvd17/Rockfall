52d883830a97c2930bb05eb63ce349b3613d32a7
Dhruv <diguvapallidhruv@gmail.com>
2025-09-16 00:30:53 +0530

    Delete rockfall directory
    Will add again soon

--- rockfall/public/js/registration.js
+++
@@ -1,8 +0,0 @@
-  (function() {
-      const form = document.querySelector('form[action="/register"]');
-      if (!form) return;
-      form.addEventListener('submit', function(e) {
-        if (!form.checkValidity()) { e.preventDefault(); e.stopPropagation(); }
-        form.classList.add('was-validated');
-      }, false);
-    })();
