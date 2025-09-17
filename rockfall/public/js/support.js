8bb56662a99d055ee5af7ea8bfa643a1aca8e1e6
Dhruv <diguvapallidhruv@gmail.com>
2025-09-17 21:32:24 +0530

    Delete rockfall directory

--- rockfall/public/js/support.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.support-option').forEach(option => {
-      option.addEventListener('click', () => {
-        alert("This would connect you to support.");
-      });
-    });
